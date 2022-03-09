7045c7819749e4fa66d65b9645df7326673e451e
hafizsyedirfanali <hafizsyedirfanali@gmail.com>
2022-03-09 23:45:57 +0530

    added new functionalities

---
+++ SyncFusionPractice/Pages/WordDoc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SyncFusionPractice.Pages
+{
+    public class WordDocModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
